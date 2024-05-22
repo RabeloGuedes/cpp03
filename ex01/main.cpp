dd904c96a5229fe0e019cf94d6168432f3261589
rabeloguedes <rabeloguedes@proton.me>
2024-05-22 13:52:38 +0100

    test: ScavTrap default constructor.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:29:49 by arabelo-          #+#    #+#             */
-/*   Updated: 2024/05/20 08:19:19 by arabelo-         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:48:26 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,17 @@
 int	main(void) {
 	std::string	fighter("Kakarot");
 	std::string	enemy("Vegetta");
-	ScavTrap	a(fighter);
-	ScavTrap	b;
 
-
-	a.attack(enemy);
-	b.attack(enemy);
-	a.attack(enemy);
-	a.takeDamage(3);
-	a.takeDamage(15);
-	b = a;
-	b.takeDamage(15);
-	a.attack(enemy);
-	a.beRepaired(10);
+	{
+		std::cout << RED_TEXT << "=================================TEST 1===============================" << std::endl;
+		std::cout << center("TESTING SCAVTRAP DEAFULT CONSTRUCTOR", 70) << std::endl << WHITE_TEXT;
+		{
+			ScavTrap	a;
+			(void)a;
+		}
+		std::cout << RED_TEXT << "=================================TEST 1================================" << std::endl;
+		std::cout << std::endl << WHITE_TEXT;
+	}
 
 	return (0);
 }
