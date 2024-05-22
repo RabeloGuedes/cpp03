5d4474bef1b8788aecddfef9e818c64bb11cf85a
rabeloguedes <rabeloguedes@proton.me>
2024-05-22 13:22:17 +0100

    tests: Test ClapTrap default constructor.

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:29:35 by arabelo-          #+#    #+#             */
-/*   Updated: 2024/05/19 11:21:37 by arabelo-         ###   ########.fr       */
+/*   Updated: 2024/05/22 08:43:39 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,32 +15,17 @@
 int	main(void) {
 	std::string	fighter("Kakarot");
 	std::string	enemy("Vegetta");
-	ClapTrap	a(fighter);
-	ClapTrap	b;
+	std::string	reader("answer");
 
-	b = a;
-
-	a.attack(enemy);
-	a.attack(enemy);
-	a.takeDamage(3);
-	a.takeDamage(15);
-	a.attack(enemy);
-	a.beRepaired(10);
-
-	std::cout << std::endl;
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-	b.attack(enemy);
-
-	std::cout << std::endl;
-	a.attack(enemy);
+	{
+		std::cout << RED_TEXT << "=================================TEST 1===============================" << std::endl;
+		std::cout << center("TESTING CLAPTRAP DEAFULT CONSTRUCTOR", 70) << std::endl << WHITE_TEXT;
+		{
+			ClapTrap	a;
+			(void)a;
+		}
+		std::cout << RED_TEXT << "=================================TEST 1================================" << std::endl;
+		std::cout << std::endl << WHITE_TEXT;
+	}
 	return (0);
 }
