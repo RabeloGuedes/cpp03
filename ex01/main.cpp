d74d18e88444c6305c0cbe5cee61ea4cd23efbe2
rabeloguedes <rabeloguedes@proton.me>
2024-05-23 07:53:53 +0100

    test: ScavTrap member functions 0.

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 13:29:49 by arabelo-          #+#    #+#             */
-/*   Updated: 2024/05/23 07:47:02 by arabelo-         ###   ########.fr       */
+/*   Updated: 2024/05/23 07:48:33 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,6 +81,29 @@ int	main(void) {
 			(void)b;
 		}
 		std::cout << BLUE_TEXT << "=================================TEST 4===============================" << std::endl;
+		std::cout << std::endl << WHITE_TEXT;
+	}
+
+	reader = "answer";
+	while (!std::cin.eof() && !reader.empty()) {
+		std::cout << "Press Enter to continue ..." << std::endl;
+		std::getline(std::cin, reader);
+	}
+
+	{
+		std::cout << MAGENTA_TEXT << "=================================TEST 5===============================" << std::endl;
+		std::cout << center("TESTING SCAVTRAP MEMBER FUNCTIONS 0", 70) << std::endl << WHITE_TEXT;
+		{
+			ScavTrap	a(fighter);
+			
+			a.attack(enemy);
+			a.takeDamage(3);
+			a.beRepaired(15);
+			a.takeDamage(45);
+			a.attack(enemy);
+			a.beRepaired(5);
+		}
+		std::cout << MAGENTA_TEXT << "=================================TEST 5===============================" << std::endl;
 		std::cout << std::endl << WHITE_TEXT;
 	}
 	return (0);
